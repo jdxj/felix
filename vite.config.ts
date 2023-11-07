e89af083badc4b25baf04017b04c1b0afee44e69
wangyq <985759262@qq.com>
2023-11-07 21:54:10 +0800

    refactor: add plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import autoImport from 'unplugin-auto-import/vite'
 import components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import unocss from 'unocss/vite'
 import banner from 'vite-plugin-banner'
 import { createHtmlPlugin } from 'vite-plugin-html'
@@ -122,6 +123,12 @@ export default defineConfig(({ mode }) => {
     },
 
     plugins: [
+      autoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      components({
+        resolvers: [ElementPlusResolver()],
+      }),
       /**
        * 支持 `.vue` 文件的解析
        */
