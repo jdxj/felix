7cd259804c04f99c05ea8dcf156166b0dc316f54
wangyq <985759262@qq.com>
2023-11-11 22:33:56 +0800

    refactor: add dep

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,25 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import App from '@/App.vue'
 import router from '@/router'
+import appCp from '@/App.vue'
 
-// 全局样式
+import { createPinia } from 'pinia'
+import persistedState from 'pinia-plugin-persistedstate'
+import elementPlus from 'element-plus'
+import * as elementPlusIcons from '@element-plus/icons-vue'
+
+
+
 import 'virtual:uno.css'
 import '@/styles/var.less'
 import '@/styles/mixin.less'
 import '@/styles/global.less'
 
-// 创建 Pinia 实例
-const pinia = createPinia()
-
-/**
- * Pinia 支持功能扩展，例如本地持久化功能
- *
- * 在命令行运行 `npm i pinia-plugin-persistedstate` 安装持久化插件
- * 在本文件里导入 `import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'`
- * 取消注释下方的这行 `pinia.use` 代码启用插件，并根据文档对单个 Store 启用配置
- *
- * @see https://prazdevs.github.io/pinia-plugin-persistedstate/zh/guide/
- */
-pinia.use(piniaPluginPersistedstate)
-
-createApp(App)
-  .use(pinia) // 启用 Pinia
+const app = createApp(appCp)
+app.use(createPinia().use(persistedState))
   .use(router)
+  .use(elementPlus)
   .mount('#app')
+
+for (const [key, component] of Object.entries(elementPlusIcons)) {
+  app.component(key, component)
+}
